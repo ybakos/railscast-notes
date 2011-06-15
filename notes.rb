786f40568b41bcb60a8d800c92c425db90715ba7
Yong Bakos <ybakos@humanoriented.com>
2011-06-15 13:52:37 -0600

    Adding notes for 168, RSS feed parsing.

--- notes.rb
+++ notes.rb
@@ -3151,6 +3151,41 @@ end
 
 
 # Railscast 168
+# Feed Parsing
+# Suggests the feedzirra gem
+# feed_entry.rb
+class FeedEntry < ActiveRecord::Base
+  def self.update_from_feed(feed_url)
+    feed = Feedzirra::Feed.fetch_and_parse(feed_url)
+    add_entries(feed.entries)
+  end
+
+  def self.update_from_feed_continuously(feed_url, delay_interval = 15.minutes)
+    feed = Feedzirra::Feed.fetch_and_parse(feed_url)
+    add_entries(feed.entries)
+    loop do
+      sleep delay_interval
+      feed = Feedzirra::Feed.update(feed)
+      add_entries(feed.new_entries) if feed.updated?
+    end
+  end
+
+  private
+
+  def self.add_entries(entries)
+    entries.each do |entry|
+      unless exists? :guid => entry.id
+        create!(
+          :name         => entry.title,
+          :summary      => entry.summary,
+          :url          => entry.url,
+          :published_at => entry.published,
+          :guid         => entry.id
+        )
+      end
+    end
+  end
+end
 
 
 
