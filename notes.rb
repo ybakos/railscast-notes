69da2bd40a8331e6ac235386d45b5eeb0baa28b2
Yong Bakos <ybakos@humanoriented.com>
2011-07-18 17:16:52 -0600

    Adding notes for 190, nokogiri.

--- notes.rb
+++ notes.rb
@@ -3724,6 +3724,35 @@ end
 
 
 # Railscast 190
+# Screen scraping with Nokogiri
+# References 173 w/ ScraAPI
+gem install nokogiri -- --with-xml2-include=/usr/local/include/libxml2 --with-xml2-lib=/usr/local/lib
+# nokogiri_test.rb
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+url = "http://www.walmart.com/search/search-ng.do?search_constraint=0&ic=48_0&search_query=batman&Find.x=0&Find.y=0&Find=Find"
+doc = Nokogiri::HTML(open(url))
+puts doc.at_css("title").text
+doc.css(".item").each do |item|
+  title = item.at_css(".prodLink").text
+  price = item.at_css(".PriceCompare .BodyS, .PriceXLBold").text[/\$[0-9\.]+/]
+  puts "#{title} - #{price}"
+  puts item.at_css(".prodLink")[:href]
+end
+# lib/tasks/product_prices.rake
+desc "Fetch product prices"
+task :fetch_prices => :environment do
+  require 'nokogiri'
+  require 'open-uri'
+  Product.find_all_by_price(nil).each do |product|
+    url = "http://www.walmart.com/search/search-ng.do?search_constraint=0&ic=48_0&search_query=#{CGI.escape(product.name)}&Find.x=0&Find.y=0&Find=Find"
+    doc = Nokogiri::HTML(open(url))
+    price = doc.at_css(".PriceCompare .BodyS, .PriceXLBold").text[/[0-9\.]+/]
+    product.update_attribute(:price, price)
+  end
+end
+# What if you have to log in to access data? Use Mechanize.
 
 
 
